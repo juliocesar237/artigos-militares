76a0e56df2f43a435d696b3d6e1688058fb1e3d1
juliocesar237 <juliocesajjr2@gmail.com>
2026-07-24 16:30:26 -0300

    Ajusta arquivos na raiz para o deploy estatico

---
+++ src/main/java/com/artigos_militares/artigos_militares/ArtigosMilitaresApplication.java
@@ -0,0 +1,13 @@
+package com.artigos_militares.artigos_militares;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArtigosMilitaresApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArtigosMilitaresApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/artigos_militares/artigos_militares/ArtigosMilitaresApplicationTests.java
@@ -0,0 +1,13 @@
+package com.artigos_militares.artigos_militares;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArtigosMilitaresApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
